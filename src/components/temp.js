de1e8b5c73143e2071389e6655ebb5aad411bbd9
Kim Brown <kbrownc341@gmail.com>
2023-12-15 21:35:48 -0500

    Board changed to only hold current play

--- src/components/temp.js
+++ src/components/temp.js
@@ -3,28 +3,35 @@ import { calculateWinner } from "../helper";
 import Board from "./Board";
 
 const App = () => {
-  const [plays, setPlays] = useState(Array(9).fill(null));
+  const [plays, setPlays] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(plays);
+  const winner = calculateWinner(plays[stepNumber]);
   const xO = xIsNext ? "X" : "O";
 
   const handleClick = (i) => {
-    const squares = [...plays];
+    const historyPoint = plays.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    // Using ... to copy an array
+    const squares = [...current];
+    // return if won or occupied
     if (winner || squares[i]) return;
+    // select square
     squares[i] = xO;
-    setPlays(squares);
+    setPlays([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
     setXisNext(!xIsNext);
   };
 
   const restart = () => {
+    setStepNumber(0);
     setXisNext('X');
-    setPlays(Array(9).fill(null));
   };
 
   return (
     <>
       <h1>Tic Tac Toe - You vs Computer</h1>
-      <Board squares={plays} onClick={handleClick} />
+      <Board squares={plays[stepNumber]} onClick={handleClick} />
       <div className="info-wrapper">
         <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
         <button onClick={() => restart()}>Restart</button>
